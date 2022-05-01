f0b7d3a9a1968ce18d50ff307c6f630eb3775625
Cade <chyeon81@icloud.com>
2022-05-02 04:26:17 +0900

    Create search-result

---
+++ src/js/search-result.js
@@ -0,0 +1,19 @@
+// search-result
+
+const tabMenu = document.querySelectorAll('.tab-menu li');
+const tabContent = document.querySelectorAll('.tab-content > div');
+
+tabMenu.forEach(function(item, i) {
+    item.addEventListener('click', function(e) {
+        e.preventDefault();
+        showContent(i);
+        moveHighlight(i);
+    });
+});
+
+function showContent(n) {
+    tabContent.forEach(function(item){
+        item.style.display = 'none';
+    });
+    tabContent[n].style.display = 'block';
+}
